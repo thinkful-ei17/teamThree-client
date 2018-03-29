5723d0cdffd5aa1bdc76dc1e7a7870e5267295e4
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-29 10:27:46 -0700

    added yearSummaryListComponent with hard-coded props

---
+++ src/components/portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import Button from './button';
+
+export class Portfolio extends React.Component{
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+const mapStateToProps = (state, props) => {
+
+}
+
+export default connect(mapStateToProps)(Portfolio)
