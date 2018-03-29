ecff8ec823de7c014c3194b429a2e7e14c6704a2
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-29 14:10:54 -0700

    made riskDetail without a pie chart

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,15 +3,15 @@ import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 import Chart from './chart';
 import Button from './button';
-import YearSummaryList from './year-summary-list';
+import RiskDetail from './risk-detail';
 import {hideLoginForm} from '../actions/users';
 import {hideRegistrationForm} from '../actions/users';
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(hideLoginForm());
-        this.props.dispatch(hideRegistrationForm());
-    }
+    // componentDidMount() {
+    //     this.props.dispatch(hideLoginForm());
+    //     this.props.dispatch(hideRegistrationForm());
+    // }
   
     handleClick = () => {
         console.log('The button was clicked');
@@ -24,7 +24,9 @@ export class Dashboard extends React.Component {
                     Username: {this.props.username}
                 </div>
                 <div className="dashboard-name">Name: {this.props.name}</div>
-                {/* <YearSummaryList/> */}
+                <RiskDetail riskType='Conservative' stocks='20' bonds='80'/>
+                <RiskDetail riskType='Moderate' stocks='50' bonds='50'/>
+                <RiskDetail riskType='Aggressive' stocks='80' bonds='20'/>
             </div>
         );
     }
---
+++ src/components/risk-detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+export default function RiskDetail(props){
+
+    return(
+       <div className='risk-detail'> 
+            <h3>{props.riskType}</h3>
+            <p className='risk-detail-units'>
+                {props.stocks}% Stocks
+            </p>
+            <p className='risk-detail-units'>
+                {props.bonds}% Bonds
+            </p>
+        </div>
+    );
+}
+
+
