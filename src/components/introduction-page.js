2100de575d48a1b8ee89d00fa658635414b9072a
Linda Foo <linda.foo.dev@gmail.com>
2018-03-29 12:19:15 -0700

    simplified introduction - working through iteration of cards

--- src/components/introduction-page.js
+++ src/components/introduction-page.js
@@ -1,32 +1,34 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
 import Button from './button';
-//import { fetchIntro } from './actions';
 
-class IntroductionPage extends React.Component {
-  componentDidMount() {
-    Promise.all([
-      //this.props.dispatch(fetchIntro()),
-      console.log('Component has mounted'),
-    ]);
+export default class IntroductionPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {cardNum: 0, introCard: [
+    {titel: 'Game', details: 'Here\'s how to play!'},
+    {title: 'Stocks', details: 'Stocks Lorem Ipsum'},
+    {title: 'Bonds', details: 'Bonds Lorem Ipsum'},
+    {title: 'Managed Fund', details: 'Managed Fund Lorem Ipsum'}]}
   }
 
   handleClick = () => {
+    this.setState(prevState => {
+      return {cardNum: prevState.cardNum + 1}
+    })
     console.log('The button was clicked');
   }
 
   render() {
     return (
-      
       <div className="introduction">
         <div className="row">
             <section className="introduction">
               <header>
-                <h1 className="title">Title Goes Here {this.props.introCard.title}</h1>
+                <h1 className="title">{this.state.introCard[this.state.cardNum].title}</h1>
               </header>
               <main>
-                <div className="introduction-details">Details Go Here {this.props.introCard.details}</div>
+                <div className="introduction-details">{this.state.introCard[this.state.cardNum].details}</div>
                 <Button name='Got It!' handleClick={this.handleClick}/>
               </main>
           </section>
@@ -35,10 +37,3 @@ class IntroductionPage extends React.Component {
     );
   }
 }
-
-const mapStateToProps = state => ({
-  //introCard: Object.assign({}, state.intro.data, { loading: state.intro.loading }),
-  introCard: {title: 'Stocks', details: 'Stocks Lorem Ipsum'},
-});
-
-export default connect(mapStateToProps)(IntroductionPage);
