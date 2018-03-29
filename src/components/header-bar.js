1d88a1b0f0739798e49bbb3aa0eee9211a9083e4
Sir Hymeninsin <johnluiscastillo@gmail.com>
2018-03-29 11:51:37 -0400

    working on nav

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {clearAuth} from '../actions/auth';
-import {showLoginForm} from '../actions/users';
-import {showRegistrationForm} from '../actions/users';
-import {clearAuthToken} from '../local-storage';
+import { connect } from 'react-redux';
+import { clearAuth } from '../actions/auth';
+import { showLoginForm } from '../actions/users';
+import { showRegistrationForm } from '../actions/users';
+import { clearAuthToken } from '../local-storage';
 import './header-bar.css';
 
 export class HeaderBar extends React.Component {
@@ -18,36 +18,67 @@ export class HeaderBar extends React.Component {
         let logOutButton;
         let loginFormNav;
         let regFormNav;
+        let appName;
+        let portfolio;
+        let investmentData;
+
 
         if (this.props.loggedIn) {
-          currentUsername = (
-           <li className="current-user">Logged in as <span className="user-span">{this.props.currentUser.username}</span></li>
-          );
+            currentUsername = (
+                <li className="current-user">Logged in as <span className="user-span">{this.props.currentUser.username}</span></li>
+            );
 
             logOutButton = (
                 <button className="red btn-logout" onClick={() => this.logOut()}>Log out</button>
             );
+
+            appName = (
+                <li className="appname">App Name</li>
+            );
         }
 
         if (!this.props.loggedIn) {
-          loginFormNav = (
-            <li className="login-text" href="#" onClick={() => this.props.dispatch(showLoginForm())} >Login</li>
-          );
-          regFormNav = (
-            <li className="login-text" href="#" onClick={() => this.props.dispatch(showRegistrationForm())}>Sign Up</li>
-          );
+            loginFormNav = (
+                <li className="login-text" href="#" onClick={() => this.props.dispatch(showLoginForm())} >Login</li>
+            );
+            regFormNav = (
+                <li className="login-text" href="#" onClick={() => this.props.dispatch(showRegistrationForm())}>Sign Up</li>
+            );
         }
-          return (
+
+        // if (this.props.loggedIn && this.props.level === 1) {
+        //     currentUsername = (
+        //         <li className="current-user">Logged in as <span className="user-span">{this.props.currentUser.username}</span></li>
+        //     );
+
+        //     logOutButton = (
+        //         <button className="red btn-logout" onClick={() => this.logOut()}>Log out</button>
+        //     );
+
+        //     appName = (
+        //         <li className="appname">App Name</li>
+        //     );
+
+        //     portfolio = (
+        //         <button className="portfolio" onClick={() => <Route exact path="/portfolio" component={portfoliothingy} />}>Portfolio</button>
+        //     );
+
+        //     investmentData = (
+        //         <button className="investment" onClick={() => <Route exact path="/investments" component={investment} />}>investmentData</button>
+        //     );
+        // }
+        return (
             <div className="header-bar">
-              <ul className="nav-bar-ul">
-               {loginFormNav}
-                {regFormNav}
-                {logOutButton}
-                {currentUsername}
+                <ul className="nav-bar-ul">
+                    {loginFormNav}
+                    {regFormNav}
+                    {appName}
+                    {logOutButton}
+                    {currentUsername}
                 </ul>
             </div>
         );
-        }
+    }
 }
 
 const mapStateToProps = state => ({
