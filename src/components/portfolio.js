e074b4e4e8ddf606f0cb9c5d4193cf4b3f450180
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-29 10:44:39 -0700

    added portfolioComponent. YearSummaryList does not exist in this branch, so I am going to comment that out in the next commit

---
+++ src/components/portfolio.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import Chart from './chart';
+import Button from './button';
+
+export function Portfolio (props){
+    const toInvestmentData = () => {
+        console.log('Links to Investment Data');
+    };
+
+    const toInvestmentForm = () => {
+        console.log('Links to InvestmentForm')
+    };
+
+    return(
+        <div className='view-port'> 
+        <h1>Your Investment Portfolio</h1>
+        <h2>Current Worth: {this.props.currentWorth}</h2>
+        <section>
+            <Chart/>
+            <YearSummaryList/>
+        </section>
+        <Button name='Review Investment Data' handleClick={toInvestmentData}/>
+        <Button name='Back to Investing!' handleClick={toInvestmentForm}/> 
+        </div> 
+    );
+}
+
+const mapStateToProps = (state, props) => ({
+    currentWorth: '$5,000' //change to state.user.currentWorth when reducer is set up
+});
+
+export default connect(mapStateToProps)(Portfolio);
