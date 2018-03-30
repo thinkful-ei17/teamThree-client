aa57ca634c4763a3211dda97a1f2eedea1e1eff8
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-30 09:44:12 -0700

    added basic marketAnalysis component without graph.  Hardcoded data will need to be changed

---
+++ src/components/market-analysis.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import Button from './button';
+
+
+export function MarketAnalysis(props){
+    let name,
+        handleClick;
+
+    if (props.year === '0'){
+        name = 'Make My First Investment!';
+        handleClick = () => {
+            console.log('This button links to Investment Form');
+        }
+    }
+    else {
+        name = 'Show Me My Investment',
+        handleClick = () => {
+            console.log('This button links to InvestmentReturn');
+        }
+    }
+
+    return(
+      <div>  
+        <h1>Market Analysis: Year {props.year}</h1>
+        <section className='market-analysis-risks'>
+        <p>Aggressive: {aggressive}% change</p>
+        <p>Moderate: {moderate}% change</p>
+        <p>Conservative: {conservative}% change</p>
+        <p>Mattress: 0.0% change</p>
+        </section>
+        <Button name={name} handleClick={handleClick}/>
+      </div>  
+    );
+}
+
+const mapStateToProps = (state, props) => ({
+    year: '1', //change to come from state
+    conservative: '0.75', //change to come from state
+    moderate: '1.2', //change to come from state
+    aggressive: '3.2' //change to come from state
+});
+
+export default connect(mapStateToProps)(MarketAnalysis)
