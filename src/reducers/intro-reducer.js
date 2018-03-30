6b08351c1dd201c4a4a01680d60cbbdae80a794a
Linda Foo <33673643+lindafoodev@users.noreply.github.com>
2018-03-30 15:41:04 -0700

    Add intro card - term info.

--- src/reducers/intro-reducer.js
+++ src/reducers/intro-reducer.js
@@ -7,9 +7,9 @@ import {
 const initialState = {
   introCard: [
     {number: 0, title: 'So You Want to Be An Investor...', details: 'There are a couple of things you should know about to get started.'},
-    {number: 1, title: 'Stocks', details: 'Stocks Lorem Ipsum Concise'},
-    {number: 2, title: 'Bonds', details: 'Bonds Lorem Ipsum Concise'},
-    {number: 3, title: 'Managed Fund', details: 'Managed Fund Lorem Ipsum Concise'}],
+    {number: 1, title: 'Stocks', details: 'A stock represents ownership or rights to a portion of a company\'s assets and profits. You are a shareholder.'},
+    {number: 2, title: 'Bonds', details: 'A bond represents you lending money to a company or government body for a specific amount and specific period of time, the amount will be returned to you with interest. You are a debtholder.'},
+    {number: 3, title: 'Managed Fund', details: 'A managed fund is managed professionally by an expert. The professional decides the  type and mix of investments in the fund. You choose to pool your money with other investors so you get access to investing opportunities that you may not have as an individual investor and diversify your risk. You own units of a fund'}],
   numCard: 0,
   introComplete: false
 };
