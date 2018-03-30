ef40b11190ae4bc799554dde196834fce1e1e360
Linda Foo <33673643+lindafoodev@users.noreply.github.com>
2018-03-30 13:24:35 -0700

    Move redirect to render
    Move the return <Redirect.../> to the render method instead of being part of componentDidMount and nextCardClick.

--- src/components/introduction-page.js
+++ src/components/introduction-page.js
@@ -7,46 +7,37 @@ import Button from './button';
 
 export class IntroductionPage extends React.Component {
   componentDidMount(){
-    if(this.props.introComplete === true){
-      console.log('already viewed intro - redirect to dashboard');
-      //risk breakdown => direct to risk breakdown
-      return <Redirect to="/dashboard"/>;
-    }
-    else{
     this.props.dispatch(showIntroCard(this.props.numCard, this.props.introCard));
     console.log('component did mount intro page');
-    }
   }
 
   nextCardClick = () => {
-    if(this.props.numCard === 3){
-      console.log('Last intro card - redirect to dashboard');
-      //risk breakdown => direct to risk breakdown
-      this.props.dispatch(hideIntroCard());
-      return <Redirect to="/dashboard"/>;
-    }
-    else{
     this.props.dispatch(nextIntroCard(this.props.numCard, this.props.introCard));
     console.log('The button was clicked in intro page');
-    }
   }
 
   render() {
-    return (
-      <div className="introduction">
-        <div className="row">
-            <section className="introduction">
-              <header>
-                <h1 className="title">{this.props.introCard[this.props.numCard].title}</h1>
-              </header>
-              <main>
-                <div className="introduction-details">{this.props.introCard[this.props.numCard].details}</div>
-                <Button name='Got It!' handleClick={this.nextCardClick}/>
-              </main>
-          </section>
+    if(this.props.numCard === 3 || this.props.introComplete) {
+      //should redirect to risk dashboard - Sam/Scott to add
+      return (<Redirect to="/dashboard"/>);
+    }
+    else {
+      return (
+        <div className="introduction">
+          <div className="row">
+              <section className="introduction">
+                <header>
+                  <h1 className="title">{this.props.introCard[this.props.numCard].title}</h1>
+                </header>
+                <main>
+                  <div className="introduction-details">{this.props.introCard[this.props.numCard].details}</div>
+                  <Button name='Got It!' handleClick={this.nextCardClick}/>
+                </main>
+            </section>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
