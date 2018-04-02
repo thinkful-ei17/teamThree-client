3921097a715118d9ec8c30b733541ccb5ef4fd87
Sam Gould <sjg326@gmail.com>
2018-04-02 15:30:53 -0700

    Update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import requiresLogin from './requires-login';
 
 import Chart from './chart';
