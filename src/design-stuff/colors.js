2cff485168a8dbb7b76ff9fa6e952ceebe9d6df7
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-04 10:11:12 -0700

    added colors.js to design-stuff folder.

---
+++ src/design-stuff/colors.js
@@ -0,0 +1,24 @@
+
+//I don't think this is going to work the way I wanted it to because it's not react-native.
+
+export const BLACK = '#0C1F27';
+
+export const WHITE = '#EDF8E8';
+
+export const DARK_BLUE = '#192C4D';
+
+export const LIGHT_BLUE = '#3478A2';
+
+export const AQUA = '#39A7B1';
+
+export const DARK_GREEN = '#2C8B39';
+
+export const MED_GREEN = '#5DCB6E';
+
+export const LIGHT_GREEN = '#B6DF9B';
+
+export const PINK = '#C24275';
+
+export const PURPLE = '#783DB8';
+
+//https://color.hailpixel.com/#0C1F27,192C4D,2C8B39,3478A2,39A7B1,B6DF9B,EDF8E8,5DCB6E,C24275,783DB8
