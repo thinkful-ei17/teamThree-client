1c48af92eea01c4009350bebf1b2a448fc3c95fd
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-05 11:12:50 -0700

    made tagline move at smallerwidths

--- src/components/splash.js
+++ src/components/splash.js
@@ -9,7 +9,27 @@ export default function Splash(props){
         <div className='splash-div'>
             <img src={confettiMan} alt='A very happy man in a cloud of confetti.'/>
             {/* <h1 className='white-text'>Centsible.</h1> */}
-                <p className='white-text'>Start Making Cents of Investing</p>
+                <p className='primary-text-color'>
+                    Start Making Cents of Investing
+                </p>
+            <section>
+                <div className='virtue'>
+                    <h2 className='primary-text-color'>
+                        Virtue 1
+                    </h2>
+                </div> 
+                <div className='virtue'>   
+                    <h2 className='primary-text-color'>
+                        Virtue 2
+                    </h2>
+                </div>
+                <div className='virtue'>
+                    <h2 className='primary-text-color'>
+                        Virtue 3
+                    </h2>
+                </div>    
+            </section>
+
         </div>
     )
 }
