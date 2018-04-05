8985eccb4648976722aa4cf08ad0f54297906de6
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-05 15:19:39 -0700

    landing page images and text are working pretty well.

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import confettiMan from '../design-stuff/CentsibleSplash/confetti-guy.jpg';
+import lineChart from '../design-stuff/CentsibleSplash/line-chart-green.svg';
+import piggyBank from '../design-stuff/CentsibleSplash/pig-blue.svg';
+import transactionIcon from '../design-stuff/CentsibleSplash/balance-aqua.svg';
 import './splash.css';
 
 export default function Splash(props){
@@ -12,21 +15,30 @@ export default function Splash(props){
                 <p className='primary-text-color'>
                     Start Making Cents of Investing
                 </p>
-            <section>
+            <section className='virtues'>
                 <div className='virtue'>
-                    <h2 className='primary-text-color'>
-                        Virtue 1
-                    </h2>
+                    <img src={piggyBank} alt='Piggy-bank'/>
+                    <span className='virtue-heading'>
+                        <h2 className='primary-text-color list-item-heading'>
+                            Learn to Invest Wisely
+                        </h2>
+                    </span>
                 </div> 
-                <div className='virtue'>   
-                    <h2 className='primary-text-color'>
-                        Virtue 2
-                    </h2>
+                <div className='virtue'>  
+                    <img src={lineChart} alt='Line chart with two lines'/>
+                    <span className='virtue-heading'> 
+                        <h2 className='primary-text-color list-item-heading'>
+                            Chart Your Growth
+                        </h2>
+                    </span>
                 </div>
                 <div className='virtue'>
-                    <h2 className='primary-text-color'>
-                        Virtue 3
-                    </h2>
+                    <img src={transactionIcon} alt='Circular arrow wrapping around a dollar sign.'/>
+                    <span className='virtue-heading'>
+                        <h2 className='primary-text-color list-item-heading'>
+                            Compare Strategies
+                        </h2>
+                    </span>    
                 </div>    
             </section>
 
