2526c27d54943e0e312f1f34e8a93223121a3c5e
Sam Gould <sjg326@gmail.com>
2018-04-05 17:24:30 -0700

    making intro into blocks of text

--- src/components/introduction-page.js
+++ src/components/introduction-page.js
@@ -33,7 +33,7 @@ export class IntroductionPage extends React.Component {
                 <h2 className="primary-heading">{this.props.introCard[this.props.numCard].title}</h2>
               </header>
               <main>
-                <h3 className="descriptive-content">{this.props.introCard[this.props.numCard].details}</h3>
+                <h3 className="descriptive-content primary-text-color intro-block-text">{this.props.introCard[this.props.numCard].details}</h3>
                 <Link to={link}>
                   <Button class='blue-button' name='Got It!' handleClick={this.nextCardClick}/>
                 </Link>
