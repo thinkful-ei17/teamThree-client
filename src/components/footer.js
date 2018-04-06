4a010c75404eb4697dfa87ce65819433db61bfed
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-06 12:09:25 -0700

    footer has basic styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,17 +3,17 @@ import './footer.css';
 
 export default function Footer(props) {
   return (
-    <footer className="background-darkest">
+    <footer className="footer">
       <div className="content-grid">
         <div>
-          Made by:
-          <ul>
+          <h4 className='footer-heading'>Made in 2018 by:</h4>
+          <ul className='footer-ul'>
             <li>
               <a
                 href="https://github.com/lindafoodev"
                 target="_blank"
                 rel="noopener noreferrer"
-                
+                className="footer-li-a"
               >
                 Linda
               </a>
@@ -24,6 +24,7 @@ export default function Footer(props) {
                 href="https://github.com/JohnLCastillo"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="footer-li-a"
               >
                 John
               </a>
@@ -34,6 +35,7 @@ export default function Footer(props) {
                 href="https://github.com/samuelgould"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="footer-li-a"
               >
                 Sam
               </a>
@@ -44,6 +46,7 @@ export default function Footer(props) {
                 href="https://github.com/ASTribble"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="footer-li-a"
               >
                 Scott
               </a>
@@ -54,6 +57,7 @@ export default function Footer(props) {
                 href="https://github.com/TriciaF"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="footer-li-a"
               >
                 Tricia
               </a>
