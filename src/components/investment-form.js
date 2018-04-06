be8ad5dc13a7098c30635119e2850db3d078e67b
Sam Gould <sjg326@gmail.com>
2018-04-06 14:03:29 -0700

    incorporating styling tweaks to investment form component

--- src/components/investment-form.js
+++ src/components/investment-form.js
@@ -26,9 +26,11 @@ export class InvestmentForm extends React.Component {
     render() {
 
         return (
-            <div className="viewport">
+            <div className="small-viewport">
                 <h2 className="primary-heading">Year {this.props.year + 1}</h2>
-                <h3 className="secondary-heading primary-text-color">Current Fund: {this.props.currentFund}</h3>
+                <h3 className="secondary-heading primary-text-color">Current Fund: ${this.props.currentFund}</h3>
+                <h3 className="secondary-heading primary-text-color">How would you like to invest this year?</h3>
+                <div className='radio-button-container'>
                     <label className="descriptive-content primary-text-color radio-button-label" htmlFor='rb1'>Aggressive
                         <input className='native-button' type='radio' name='strategy' id='rb1' value='high' onChange={this.onChange.bind(this)} />
                         <span className='custom-radio-button'></span>
@@ -45,11 +47,12 @@ export class InvestmentForm extends React.Component {
                         <input className='native-button' type='radio' name='strategy' id='rb4' value='mattress' onChange={this.onChange.bind(this)} />
                         <span className='custom-radio-button'></span>
                     </label>
-                  <div>
+                </div>
+                <div className='center-object'>
                     <Link to='/market-analysis'>
                         <Button class='blue-button' name='Invest' handleClick={this.invest} />
                     </Link>   
-                  </div>
+                </div>
             </div>
         );
     }
