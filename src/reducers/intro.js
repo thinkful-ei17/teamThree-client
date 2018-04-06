cb1d7837315c1aeb93f0f14c79a72470b2a65ea9
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-06 14:11:35 -0700

    split out details in introReducer state, added separate paragraphs to the intro cards, worked on the css

--- src/reducers/intro.js
+++ src/reducers/intro.js
@@ -6,10 +6,10 @@ import {
 
 const initialState = {
   introCard: [
-    {number: 0, title: 'So You Want to Be An Investor...', details: 'There are a couple of things you should know about to get started.'},
-    {number: 1, title: 'Stocks', details: 'A stock represents ownership or rights to a portion of a company\'s assets and profits. You are a shareholder.'},
-    {number: 2, title: 'Bonds', details: 'A bond represents you lending money to a company or government body for a specific amount and specific period of time, the amount will be returned to you with interest. You are a debtholder.'},
-    {number: 3, title: 'Managed Fund', details: 'A managed fund is managed professionally by an expert. The professional decides the  type and mix of investments in the fund. You choose to pool your money with other investors so you get access to investing opportunities that you may not have as an individual investor and diversify your risk. You own units of a fund'}],
+    {number: 0, title: 'So You Want to Be An Investor...', detailA: 'There are a couple of things you should know about to get started.', detailC:'Stocks & Bonds'},
+    {number: 1, title: 'Stocks', detailA: 'A stock represents ownership or rights to a portion of a company\'s assets and profits.', detailC:'You are a shareholder.'},
+    {number: 2, title: 'Bonds', detailA: 'A bond represents you lending money to a company or government body for a specific amount and specific period of time.', detailB:'The amount of money you lend will be returned to you with interest.', detailC:'You are a debtholder.'},
+    {number: 3, title: 'Managed Fund', detailA: 'A managed fund is managed professionally by an expert. The professional decides the  type and mix of investments in the fund.', detailB: 'You choose to pool your money with other investors so you get access to investing opportunities that you may not have as an individual investor and to diversify your risk.', detailC: 'You own units of a fund.'}],
   numCard: 0,
   introComplete: false
 };
